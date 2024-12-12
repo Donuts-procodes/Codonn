922dadb9cdb1e66c5107ffbecf0afca11f67f75e
Donuts-procodes <151659340+Donuts-procodes@users.noreply.github.com>
2024-12-12 13:50:06 +0530

    fixed and trying again

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  base: '/react-gh-pages/',
+  base: '/react-gh-pages/', // Adjust this to match your GitHub repository name
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-  },
 });
